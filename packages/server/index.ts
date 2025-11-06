d39f2ec4debb8a02e92ae0fc2e888ffcd2690a12
SanjanaAbeykoon7 <sanjanaabeykoon1@gmail.com>
2025-11-06 12:11:16 +0530

    Set up Prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,13 +8,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from the server package!');
+   res.send('Hello from the server package!');
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({ message: 'Hello, API!' });
+   res.json({ message: 'Hello, API!' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });

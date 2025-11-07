d703a463b5ffa41eeb25a8f8048f5ae8311317b4
SanjanaAbeykoon7 <sanjanaabeykoon1@gmail.com>
2025-11-07 09:11:43 +0530

    frontend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import { ChatInterface } from './components/ChatInterface';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
-   return (
-      <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>
-      </div>
-   );
+   return <ChatInterface />;
 }
 
 export default App;
